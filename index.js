85aee73898a259199342786eecdecad0a691ac53
capelliludivine <ludivine@kbdev.io>
2021-10-08 10:14:16 +0200

    Commit resolution d'erreur

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 class Yahtzee {
   constructor() {}
 
-  roll() {}
+  roll() {
+    let dice = [];
+
+    for (var i = 0; i < 5; ++i) {
+      dice[i] = Math.floor(Math.random() * 6) + 1;
+    }
+    console.log(dice);
+    return dice;
+  }
 }
 
 module.exports = Yahtzee;
--- index.test.js
+++ index.test.js
@@ -1,26 +1,27 @@
 const Yahtzee = require("./index");
-const assert = require("assert").strict;
-const expect = require("chai").expect;
+var chai = require("chai");
+var assert = chai.assert; // Using Assert style
+var expect = chai.expect; // Using Expect style
+var should = chai.should();
 
 describe("test", function () {
   let yahtzee = Yahtzee;
-  var expect = chai.expect;
 
   it("should return the result of dice roll", function () {
     let dices = [1, 3, 3, 4, 6];
     expect(yahtzee.roll()).toEqual(dices);
   }); // Je teste le lancé de dé
 
-  //   it("should return the result of dice roll with the saving of dice", function () {
-  //     let d1 = 1;
-  //     let d2 = 3;
-  //     let d3 = 3;
-  //     let d4 = 4;
-  //     let d5 = 6;
-  //     let save = [d1, d2];
+  it("should return the result of dice roll with the saving of dice", function () {
+    let d1 = 1;
+    let d2 = 3;
+    let d3 = 3;
+    let d4 = 4;
+    let d5 = 6;
+    let save = [d1, d2];
 
-  //     // yahtzee.rollWithSave(save);
-  //   }); // Je teste le lancé de dé avec des dés sauvegardés
+    yahtzee.rollWithSave(save);
+  }); // Je teste le lancé de dé avec des dés sauvegardés
 
   //     expect(yahtzee.sauvegarde(d)).toBe([]);
   //   }); // Je teste la sauvegarde d'un dé
